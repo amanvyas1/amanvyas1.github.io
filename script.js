973bf54af54a290179110a33f621497e9a077c45
Aman Vyas <aman.vyas.88864@gmail.com>
2022-09-12 00:08:39 +0530

    navbar and intro added

---
+++ script.js
@@ -0,0 +1,14 @@
+const ham = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+ham.addEventListener("click",() => {
+    ham.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav-link").forEach(n =>{
+    n.addEventListener("click",() => {
+        ham.classList.remove("active");
+        navMenu.classList.remove("active");
+    })
+})
